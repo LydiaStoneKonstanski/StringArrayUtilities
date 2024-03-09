80bcc6081372c9988b0d4e67f2440cfad79ddb27
LydiaStoneKonstanski <yourname@example.com>
2024-03-09 13:10:21 -0500

    Added methods contains through removeConsecutiveDuplicates

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington;
-import java.lang.String.split;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -12,9 +13,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        String [] intoWords = array.split(" ");
-
-        return intoWords[0];
+        return array[0];
     }
 
 
@@ -23,8 +22,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        String[] sentenceToWords = array.split(" ");
-            return sentenceToWords [1];
+            return array [1];
     }
 
     /**
@@ -32,21 +30,16 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        String[] sentenceToWords = array.split(" ");
-        if (sentenceToWords.length > 0) {
-            return array[sentenceToWords.length - 1];
-        }
+        return array[array.length - 1];
     }
+
 
     /**
      * @param array array of String objects
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        String[] sentenceToWords = array.split(" ");
-        if (array.length > 0) {
-            return array[array.length - 2];
-        }
+        return array[array.length - 2];
     }
 
     /**
@@ -57,94 +50,156 @@ public class StringArrayUtils {
     public static boolean contains(String[] array, String value) {
         int i;
         String x;
-        String[] sentenceToWords = array.split(" ");
-        for (i = 0; i <= sentenceToWords.length; i++) {
-            String x = sentenceToWords[i];
+        for (i = 0; i <= array.length; i++) {
+            x = array[i];
+            if (x.equals(value)) {
+                return true;
+            }
         }
-        if (x.equals(value)) {
+        return false;
+    }
+
+    /**
+     * @param array of String objects
+     * @return an array with identical contents in reverse order
+     */ // TODO
+    public static String[] reverse (String[]array){
+        String[] b = new String[array.length];
+        int j = array.length;
+        for (int i = 0; i < array.length; i++) {
+            b[j - 1] = array[i];
+            j = j - 1;
+        }
+        return b;
+    }
+
+    /**
+     * @param array array of String objects
+     * @return true if the order of the array is the same backwards and forwards
+     */ // TODO
+    public static boolean isPalindromic (String[]array){
+        String[] reversedStringArray = reverse(array);
+        if(reversedStringArray.equals(array)){
             return true;
         }
+        return false;
+    }
 
-        public static boolean contains (String[]array, String value){
-            String check;
-            for (int i = 0; i <= array.length; i++) {
-                String[] sentenceToWords = array.split(" ");
-                String check = sentenceToWords[i];
-                if (check.equals(value)) {
-                    return true;
+    /**
+     * @param array array of String objects
+     * @return true if each letter in the alphabet has been used in the array
+     */ // TODO
+    public static boolean isPangramic (String[]array){
+        //iterate through contents of each string in the array
+        //check all the characters could turn all the characters into one string,
+        //or nest for each string in array and each character in string.
+        //compare to alphabet.
+        //if alphabet letter does not appear in the strings, false.
+        //else return true.
+
+        // Iterate through array
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for (Character letter : alphabet.toCharArray()) {
+            Boolean foundLetter = false;
+            for (String item : array) {
+                for (Character c : item.toCharArray()) {
+                    // Add logic for true
+                    if(letter.equals(c)){
+                        foundLetter = true;
+                    }
                 }
             }
-
-            /**
-             * @param array of String objects
-             * @return an array with identical contents in reverse order
-             */ // TODO
-            public static String[] reverse (String[]array){
-                return null;
-            }
-
-            /**
-             * @param array array of String objects
-             * @return true if the order of the array is the same backwards and forwards
-             */ // TODO
-            public static boolean isPalindromic (String[]array){
-                return false;
-            }
-
-            /**
-             * @param array array of String objects
-             * @return true if each letter in the alphabet has been used in the array
-             */ // TODO
-            public static boolean isPangramic (String[]array){
-                return false;
-            }
-
-            /**
-             * @param array array of String objects
-             * @param value value to check array for
-             * @return number of occurrences the specified `value` has occurred
-             */ // TODO
-            public static int getNumberOfOccurrences (String[]array, String value){
-                return 0;
-            }
-
-            /**
-             * @param array         array of String objects
-             * @param valueToRemove value to remove from array
-             * @return array with identical contents excluding values of `value`
-             */ // TODO
-            public static String[] removeValue (String[]array, String valueToRemove){
-                return null;
-            }
-
-            /**
-             * @param array array of chars
-             * @return array of Strings with consecutive duplicates removes
-             */ // TODO
-            public static String[] removeConsecutiveDuplicates (String[]array){
-                return null;
-            }
-
-            /**
-             * @param array array of chars
-             * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
-             */ // TODO
-            public static String[] packConsecutiveDuplicates (String[]array){
-                return null;
-            }
-
-
-            private static int[] getRandomArray(int len) {
-
-                Random random = new Random();
-                int[] newInt = new int[len];
-                for (int i = 0; i < len; i++) {
-                    newInt[i] = random.nextInt(100);
-                }
-                return newInt;
-            }
-            int[] firstArray = getRandomArray(len 10);
-            System.out.println(Arrays.toString(firstArray));
+            return false;
         }
+        return true;
+    }
+
+    /**
+     * @param array array of String objects
+     * @param value value to check array for
+     * @return number of occurrences the specified `value` has occurred
+     */ // TODO
+    public static int getNumberOfOccurrences (String[]array, String value){
+        //need counter for occurrences
+        Integer counter = 0;
+        //iterate through array
+        //compare to value,
+        //each time value is found, trip the counter, return counter.
+        for(String eachItem: array){
+            if (eachItem.equals(value)){
+                counter++;
+            }
         }
+        return counter;
+    }
+
+    /**
+     * @param array         array of String objects
+     * @param valueToRemove value to remove from array
+     * @return array with identical contents excluding values of `value`
+     */ // TODO
+    public static String[] removeValue (String[]array, String valueToRemove){
+        //create an empty array list
+        List<String> list = new ArrayList<String>();
+        for(String eachItem : array){
+            //iterate through the array, if item in array != valueToRemove,
+            // store in array list
+            if(!eachItem.equals(valueToRemove)){
+                list.add(eachItem);
+            }
         }
+        //convert array list to array at the end.
+        return (String[])list.toArray();
+    }
+
+    /**
+     * @param array array of chars
+     * @return array of Strings with consecutive duplicates removes
+     */ // TODO
+    public static String[] removeConsecutiveDuplicates (String[]array){
+        //convert to array list,
+        List<String> list = new ArrayList<String>();
+        //iterate through array,
+        //if same, don't add current item,otherwise do add it,
+        for(int i = 0; i <= array.length-2; i++){
+            String eachItem = array[i];
+            String nextItem = array[i + 1];
+            if(!eachItem.equals(nextItem)){
+                list.add(eachItem);
+            }
+        }
+        //convert back to array
+        //return new
+        return (String[])list.toArray();
+    }
+
+    /**
+     * @param array array of chars
+     * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
+     */ // TODO
+    public static String[] packConsecutiveDuplicates (String[]array){
+        //iterate through array,
+        //if same, send to string with +,
+        //create an array with index of 0,
+        //put the string into the singleIndexArray
+        //return singleIndexArray.
+        return null;
+    }
+
+
+    // This is a useful function for future reference
+    private static int[] getRandomArray(int len) {
+
+        Random random = new Random();
+        int[] newInt = new int[len];
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(100);
+        }
+        return newInt;
+    }
+
+            // Notes - this is useful information:
+//            int[] firstArray = getRandomArray(len 10);
+//            System.out.println(Arrays.toString(firstArray));
+}
